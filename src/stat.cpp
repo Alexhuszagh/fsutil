a51f8f0cc3c6d1c951c6cf27a18cd4c1e4a9424e
Alex Huszagh <ahuszagh@ahuszagh.localdomain>
2017-06-16 17:16:47 -0700

    Enhancements for cross-platform stat

--- src/stat.cpp
+++ src/stat.cpp
@@ -13,6 +13,14 @@
 
 namespace fsutil
 {
+// MACROS
+// ------
+
+/**
+ *  Make error code from descriptor.
+ */
+#define MAKE_ERROR(name) std::make_error_code(std::errc::name)
+
 // DETAIL
 // ------
 
@@ -25,18 +33,28 @@ static void handle_error(const path_t& path, int code)
         case 0:
             return;
         case ENOENT:
-            throw fs::filesystem_error("File not found.", path, std::erc::no_such_file_or_directory);
+            throw fs::filesystem_error("File not found.", path, MAKE_ERROR(no_such_file_or_directory));
          case EINVAL:
-//            throw fs::filesystem_error("Invalid parameter to _stat.", path, code);
+            throw fs::filesystem_error("Invalid parameter to _stat.", path, MAKE_ERROR(invalid_argument));
          default:
-            throw fs::filesystem_error("Unexpected error in _stat.", path);
+            throw fs::filesystem_error("Unexpected error in stat.", path, MAKE_ERROR(not_supported));
     }
 }
 
 
-static void copy_native(const _stat& src, stat_t& dst)
+static void copy_native(const struct _stat& src, stat_t& dst)
 {
-    // TODO: here....
+    dst.st_dev = src.st_dev;
+    dst.st_ino = src.st_ino;
+    dst.st_mode = src.st_mode;
+    dst.st_nlink = src.st_nlink;
+    dst.st_uid = src.st_uid;
+    dst.st_gid = src.st_gid;
+    dst.st_rdev = src.st_rdev;
+    dst.st_size = src.st_size;
+//    dst.st_atime = src.st_atime;
+//    dst.st_mtime = src.st_mtime;
+//    dst.st_ctime = src.st_ctime;
 }
 
 
@@ -44,7 +62,10 @@ stat_t stat_windows(const path_t& path)
 {
     struct _stat sb;
     stat_t data;
-    handle_error(::_stat(path.wstring().data(), &sb));
+    int code;
+
+    code = ::_stat(path.wstring().data(), &sb);
+    handle_error(path, code);
     copy_native(sb, data);
 
     return data;
@@ -58,20 +79,32 @@ static void handle_error(const path_t& path, int code)
         case 0:
             return;
         case ENOENT:
-            throw fs::filesystem_error("File not found.", path, std::erc::no_such_file_or_directory);
+            throw fs::filesystem_error("File not found.", path, MAKE_ERROR(no_such_file_or_directory));
         case EINVAL:
-//            throw fs::filesystem_error("Invalid parameter to stat.", path, code);
+            throw fs::filesystem_error("Invalid parameter to stat.", path, MAKE_ERROR(invalid_argument));
         case ENOMEM:
-//            throw fs::filesystem_error("Out of memory.", path, code);
+            throw fs::filesystem_error("Out of memory.", path, MAKE_ERROR(not_enough_memory));
         default:
-            throw fs::filesystem_error("Unexpected error in stat.", path);
+            throw fs::filesystem_error("Unexpected error in stat.", path, MAKE_ERROR(not_supported));
     }
 }
 
 
-static void copy_native(const stat& src, stat_t& dst)
+static void copy_native(const struct stat& src, stat_t& dst)
 {
-    // TODO: here....
+    dst.st_dev = src.st_dev;
+    dst.st_ino = src.st_ino;
+    dst.st_mode = src.st_mode;
+    dst.st_nlink = src.st_nlink;
+    dst.st_uid = src.st_uid;
+    dst.st_gid = src.st_gid;
+    dst.st_rdev = src.st_rdev;
+    dst.st_size = src.st_size;
+// TODO: these are macros for st_atim, etc.
+// Need to make sure they're compatible... Fuck
+//    dst.st_atime = src.st_atime;
+//    dst.st_mtime = src.st_mtime;
+//    dst.st_ctime = src.st_ctime;
 }
 
 
@@ -79,7 +112,10 @@ stat_t stat_posix(const path_t& path)
 {
     struct stat sb;
     stat_t data;
-    handle_error(::stat(path.c_str(), &sb));
+    int code;
+
+    code = ::stat(path.c_str(), &sb);
+    handle_error(path, code);
     copy_native(sb, data);
 
     return data;
